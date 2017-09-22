0873aa2c40d090076d96c917b3b64afe56c8dc3f
wmanka <wioletta.manka728@gmail.com>
2017-09-22 16:48:22 +0200

    Show all items using jquery.datatable and API

---
+++ OnlineShoppingApp/Controllers/API/ItemsController.cs
@@ -0,0 +1,27 @@
+﻿using OnlineShoppingApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.Entity;
+
+namespace OnlineShoppingApp.Controllers.API
+{
+    public class ItemsController : ApiController
+    {
+        private ApplicationDbContext context;
+
+        public ItemsController()
+        {
+            context = new ApplicationDbContext();
+        }
+
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            return context.Items.Include(m => m.Category).ToList();
+        }
+    }
+}
