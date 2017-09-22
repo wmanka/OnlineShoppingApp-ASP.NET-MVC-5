79ec7c85b9261a5cf2eb05e8fd3f828e98fcc591
wmanka <wioletta.manka728@gmail.com>
2017-09-22 16:12:53 +0200

    Add new item

---
+++ OnlineShoppingApp/Controllers/ItemsController.cs
@@ -0,0 +1,62 @@
+﻿using OnlineShoppingApp.Models;
+using OnlineShoppingApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShoppingApp.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private ApplicationDbContext context;
+        public ItemsController()
+        {
+            context = new ApplicationDbContext();
+        }
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            var viewModel = new ItemFormViewModel()
+            {
+                Item = new Item(),
+                Categories = context.Categories.ToList()
+            };
+
+            return View("ItemForm", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Item item)
+        {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new ItemFormViewModel()
+                {
+                    Item = item,
+                    Categories = context.Categories.ToList()
+                };
+
+                return View("ItemForm", viewModel);
+            }
+
+            if (item.Id == 0)
+            {
+                context.Items.Add(item);
+            }
+
+            context.SaveChanges();
+
+            return RedirectToAction("Index", "Items");
+
+        }
+    }
+}
