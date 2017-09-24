fc62243e042047d1933cf8eb2781a48674be45c1
wmanka <wioletta.manka728@gmail.com>
2017-09-25 01:19:10 +0200

    Manage wishlist items using ajax and add toastr

--- OnlineShoppingApp/App_Start/BundleConfig.cs
+++ OnlineShoppingApp/App_Start/BundleConfig.cs
@@ -14,7 +14,8 @@ namespace OnlineShoppingApp
                         "~/Scripts/bootbox.js",
                         "~/Scripts/respond.js",
                         "~/Scripts/datatables/jquery.datatables.js",
-                        "~/Scripts/datatables/datatables.bootstrap.js"
+                        "~/Scripts/datatables/datatables.bootstrap.js",
+                        "~/Scripts/toastr.js"
                         ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
@@ -28,7 +29,9 @@ namespace OnlineShoppingApp
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/datatables/css/datatables.bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/toastr.css"
+                      ));
         }
     }
 }
